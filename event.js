d195593a90baa7aa0fb0f375e4d1d257d6560c66
ssmohanty153 <ssmohanty153@gmail.com>
2021-12-28 12:24:18 +0530

    set interval and set timeOut

---
+++ event.js
@@ -0,0 +1,11 @@
+// console.log("first task");
+// setTimeout(()=>{
+//     console.log("second task");
+// },2000)
+// console.log("next task");
+
+console.log("first task");
+setInterval(()=>{
+    console.log("second task");
+},5000)
+console.log("next task");
